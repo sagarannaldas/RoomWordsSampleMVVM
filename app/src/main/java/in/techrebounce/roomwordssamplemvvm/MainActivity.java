6febe7d275629d94e00c5b029509c284dca0f79f
sagar <annal7057@gmail.com>
2021-02-19 13:35:56 +0530

    RecyelerView adapter

--- app/src/main/java/in/techrebounce/roomwordssamplemvvm/MainActivity.java
+++ app/src/main/java/in/techrebounce/roomwordssamplemvvm/MainActivity.java
@@ -1,8 +1,12 @@
 package in.techrebounce.roomwordssamplemvvm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import in.techrebounce.roomwordssamplemvvm.adapter.WordListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/in/techrebounce/roomwordssamplemvvm/adapter/WordListAdapter.java
@@ -0,0 +1,42 @@
+package in.techrebounce.roomwordssamplemvvm.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+import in.techrebounce.roomwordssamplemvvm.db.Word;
+import in.techrebounce.roomwordssamplemvvm.viewholder.WordViewHolder;
+
+public class WordListAdapter extends ListAdapter<Word, WordViewHolder> {
+
+    public WordListAdapter(@NonNull DiffUtil.ItemCallback<Word> diffCallback) {
+        super(diffCallback);
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return WordViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        Word current = getItem(position);
+        holder.bind(current.getWord());
+    }
+
+    public static class WordDiff extends DiffUtil.ItemCallback<Word> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Word oldItem, @NonNull Word newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Word oldItem, @NonNull Word newItem) {
+            return oldItem.getWord().equals(newItem.getWord());
+        }
+    }
+}
