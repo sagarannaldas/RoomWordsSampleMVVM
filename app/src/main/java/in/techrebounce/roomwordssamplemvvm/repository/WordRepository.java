83dc2b59ccce158665ecb46ab48d11cd27ae4efa
sagar <annal7057@gmail.com>
2021-02-19 10:27:58 +0530

    AndroidViewModel integration

--- app/src/main/java/in/techrebounce/roomwordssamplemvvm/repository/WordRepository.java
+++ app/src/main/java/in/techrebounce/roomwordssamplemvvm/repository/WordRepository.java
@@ -19,7 +19,7 @@ public class WordRepository {
     // dependency. This adds complexity and much more code, and this sample is not about testing.
     // See the BasicSample in the android-architecture-components repository at
     // https://github.com/googlesamples
-    WordRepository(Application application) {
+    public WordRepository(Application application) {
         WordRoomDataBase db = WordRoomDataBase.getDataBase(application);
         mWordDao = db.wordDao();
         mAllWords = mWordDao.getAlphabetizedWords();
@@ -27,13 +27,13 @@ public class WordRepository {
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
-    LiveData<List<Word>> getAllWords() {
+    public LiveData<List<Word>> getAllWords() {
         return mAllWords;
     }
 
     // You must call this on a non-UI thread or your app will throw an exception. Room ensures
     // that you're not doing any long running operations on the main thread, blocking the UI.
-    void insert(Word word) {
+    public void insert(Word word) {
         WordRoomDataBase.databaseWriteExecutor.execute(() -> {
             mWordDao.insert(word);
         });
---
+++ app/src/main/java/in/techrebounce/roomwordssamplemvvm/viewmodel/WordViewModel.java
@@ -0,0 +1,35 @@
+package in.techrebounce.roomwordssamplemvvm.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+import in.techrebounce.roomwordssamplemvvm.db.Word;
+import in.techrebounce.roomwordssamplemvvm.repository.WordRepository;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mWordRepository;
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+        mWordRepository = new WordRepository(application);
+        mAllWords = mWordRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mWordRepository.insert(word);
+    }
+
+}
